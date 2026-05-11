7186ec3daff8f9961e21c68fb6f10e509afbf313
OskarPaul93 <paul_1993oscar@hotmail.com>
2026-05-10 22:08:39 -0500

    Botones Parametros y Clientes para que aparezca inf

--- simulador_completo.js
+++ simulador_completo.js
@@ -11,3 +11,35 @@
 
   
 //Para recuperar o mostrar información usar los métodos de la clase utilitarios, puede agregar métodos adicionales en utilitarios
+
+//document.getElementById("parametros").classList.add("activa");
+//document.getElementById("clientes").classList.add("activa");
+//document.getElementById("parametros").classList.remove("activa");
+
+function ocultarSecciones(){
+  let componente= document.getElementById("parametros");
+  let listaClass = componente.classList;
+  listaClass.remove("activa");  // oculta
+
+  // se puede poner:
+  //function ocultarSecciones(){
+  //document.getElementById("parametros").classList.remove("activa");
+  //document.getElementById("clientes").classList.remove("activa");
+//}
+ 
+  let componente2 = document.getElementById("clientes");
+  let listaClass2 = componente2.classList;
+  listaClass2.remove("activa");
+
+
+}
+
+function mostrarSeccion(id) { // funcion q activa parte visual
+    ocultarSecciones();
+    let componente= document.getElementById(id);
+    let listaClass = componente.classList;
+    listaClass.add("activa");  //activa
+}
+
+mostrarSeccion("parametros");
+mostrarSeccion("clientes");
